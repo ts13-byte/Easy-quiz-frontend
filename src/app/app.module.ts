88943b4e5a6b7dde5b57f2b829fcf057018caa61
Sara Tripathi <saratripathi0924@gmail.com>
2023-07-17 15:19:18 +0530

    Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import Swal from 'sweetalert2';
+import { HomeComponent } from './pages/home/home.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   
@@ -19,17 +27,23 @@ import { LoginComponent } from './pages/login/login.component';
     NavbarComponent,
     FooterComponent,
     SignupComponent,
-    LoginComponent
-  ],
+    LoginComponent,
+    HomeComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    MatFormFieldModule
-
-  ],
+    MatFormFieldModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+constructor(private userService: UserService,private snack:MatSnackBar){}
+
+public user={
+    userName:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'',
+    enabled:'',
+    image:''
+
+}
+ngOnInit():void {}
+formSubmit(){
+  console.log(this.user);
+  if(this.user.userName=='' || this.user.userName==null){
+    this.snack.open("UserName is required","OK",{
+      duration:3000,
+      verticalPosition:'top',
+     
+    });
+    return;
+  }
+  //addUser: userService
+  this.userService.addUser(this.user).subscribe(
+    (data:any)=>{
+        //success
+        console.log(data);
+        Swal.fire('Registeration complete!!','User id is : '+data.id,'success');
+    },
+    (error)=>{
+        //error
+        console.log(error);
+        this.snack.open('something went wrong','',{
+          duration:3000,
+          verticalPosition:'top',
+        });
+    }
+  )
+}
 
 }
